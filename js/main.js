1b0cecc3bcb0da2cafc6ac19d1739ccee24807d3
Hector Melendez <hector.melendez129@gmail.com>
2025-10-05 21:58:40 -0400

    052155ROCT25 updated style, background photo-weather logic, fixed comments and spacing for clarity

--- js/main.js
+++ js/main.js
@@ -1,62 +1,73 @@
-//The user will enter a city, state, country. 
-// Use city, staatae country to get the weather
-// api key for nasa:   d23e009d9e3c420522a7d9b1300082d7
-/*
-
-create const for button doc.qs
-activate button with .addevli(click, func)
-
-grab text value, convert to city, state, country
-
-create function called getWeather
-* Create the following inside of the function
-** create the const url with backtick, set interpolation for date
-** create the fetch call
-***write data.title inside of the h2 and img-alt
-***set img-src to data.url
-***set h3 to data.explanation
-*** grab date value, convert to yyyy-mm-dd
-*/
-
-
+//Activate button, add event listener
 const getLocation = document.querySelector('button')
 getLocation.addEventListener('click', getWeather)
 
 function getWeather() {
 
-    //get units
+    // Get units from radio button
     const unit = document.querySelector('input[name = unit]:checked').value
 
-    //creaate const userdata, api
+    // Creaate const userdata, api, country
     const city = document.querySelector('input').value
     const api = `d23e009d9e3c420522a7d9b1300082d7`
     const country = document.querySelector('#country').value
 
-    //insert date into url
+    // Insert date into url
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api}&units=${unit}`
     fetch(url)
         .then(res => res.json()) //parse response into aa json
         .then(data => {
-            //logs json, title, url, explanations is description
+            // Logs json, title, url, explanations is description
             console.log(data)
-            console.log(data.name)
-            console.log(data.main)
-            console.log(data.weather[0].description)
 
-            //display city name to dom          
+            // Display city name to dom          
             document.querySelector('#city').innerText = `Location: ${data.name} `
 
-            //display temp and unit
+            // Display temp and unit
             if (unit === 'imperial') {
                 document.querySelector('#currentTemp').innerText = `Current Temperature: ${Math.round(data.main.temp)} ° F`
             } else {
                 document.querySelector('#currentTemp').innerText = `Current Temperature: ${Math.round(data.main.temp)} ° C`
             }
 
-            //display weather
+            // Display weather
             document.querySelector('#weather').innerText = `Current Weather: ${data.weather[0].description.toUpperCase()}`
+
+            // Change background based on weather description
+            const weatherDesc = data.weather[0].main.toLowerCase()
+            const body = document.body
+
+            // Reset all classes
+            body.className = ''
+
+            if (weatherDesc.includes('cloud')
+                || weatherDesc.includes('overcast')) {
+
+                body.classList.add('cloudy')
+
+            } else if (weatherDesc.includes('rain')
+                || weatherDesc.includes('drizzle')
+                || weatherDesc.includes('thunderstorm')
+            ) {
+
+                body.classList.add('rainy')
+
+            } else if (weatherDesc.includes('snow')) {
+
+                body.classList.add('snowy')
+
+            } else if (weatherDesc.includes('clear')) {
+
+                body.classList.add('sunny')
+
+            } else {
+
+                body.classList.add('defaultWeather')
+
+            }
         })
         .catch(err => {
             console.log(`error ${err}`)
-        })
+        }
+    )
 }
